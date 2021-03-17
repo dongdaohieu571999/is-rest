b07123c7943520c730f4b2f7fc605db3e364b13c
dongdaohieu <daohieudong571999@gmail.com>
2021-03-17 10:16:22 +0700

    update illustration

---
+++ src/main/java/com/is/issystem/repository/IllustrationRepository.java
@@ -0,0 +1,7 @@
+package com.is.issystem.repository;
+
+import com.is.issystem.entities.Illustration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IllustrationRepository extends JpaRepository<Illustration,Integer> {
+}
---
+++ src/main/java/com/is/issystem/repository/IllustrationSubInterestRepository.java
@@ -0,0 +1,7 @@
+package com.is.issystem.repository;
+
+import com.is.issystem.entities.IllustrationSubInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IllustrationSubInterestRepository extends JpaRepository<IllustrationSubInterest,Integer> {
+}
