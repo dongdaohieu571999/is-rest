d0dca9cc79548e4c85ec8d8e3359edc535a6dad6
eechooo789 <62242038+eechooo789@users.noreply.github.com>
2021-03-22 19:38:57 +0700

    fix

--- src/main/java/com/is/issystem/repository/EmployeeDTORepository.java
+++ src/main/java/com/is/issystem/repository/EmployeeDTORepository.java
@@ -13,6 +13,6 @@ public interface EmployeeDTORepository extends JpaRepository<EmployeeDTO,Integer
     @Query(value = "select ei.id,ei.date_of_birth,ei.email,ei.phone,ei.ID_card,ei.id_acc,ea.code,ei.name,ei.dept_id,de.in_dept \n" +
             "from is_agency_db.employee_acc as ea \n" +
             "right join is_agency_db.employee_info as ei on ea.id = ei.id_acc\n" +
-            "left join is_agency_db.department as de on de.id = ei.dept_id ",nativeQuery = true)
+            "left join is_agency_db.department as de on de.id = ei.dept_id order by id desc",nativeQuery = true)
     List<EmployeeDTO> getEmployeeInfo();
 }
