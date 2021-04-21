94b8656ffa6d9335a08cdfd2aef4621ed28f2f6a
eechooo789 <62242038+eechooo789@users.noreply.github.com>
2021-04-21 15:22:03 +0700

    abc

--- src/main/java/com/is/issystem/repository/entity_dto_repository/IllustrationItemOfListRepository.java
+++ src/main/java/com/is/issystem/repository/entity_dto_repository/IllustrationItemOfListRepository.java
@@ -9,12 +9,12 @@ import java.util.List;
 
 public interface IllustrationItemOfListRepository extends JpaRepository<IllustrationItemOfList,Integer> {
     @Query(value = "SELECT ill.*,mi.benifit_name FROM is_agency_db.illustration as ill INNER JOIN is_agency_db.illustration_main_benifit\n" +
-            "            as imi on ill.id = imi.id_illustration INNER JOIN is_agency_db.main_benifit as mi on mi.id = imi.id_main_benifit\n" +
-            "            LEFT JOIN contract as c on c.id_illustration = ill.id  where ill.id_customer_info = ?1 and c.id is null order by id desc",nativeQuery = true)
+            "                        as imi on ill.id = imi.id_illustration INNER JOIN is_agency_db.main_benifit as mi on mi.id = imi.id_main_benifit\n" +
+            "                        LEFT JOIN contract as c on c.id_illustration = ill.id  where ill.id_customer_info = ?1 and c.id is null order by id desc",nativeQuery = true)
     List<IllustrationItemOfList> listIllustrationCustomerOwn(int id);
 
     @Query(value = "SELECT  ill.*,mi.benifit_name FROM is_agency_db.illustration as ill \n" +
-            "            INNER JOIN is_agency_db.illustration_main_benifit as imi on ill.id = imi.id_illustration \n" +
+            "            INNER JOIN is_agency_db.illustration_main_intserest as imi on ill.id = imi.id_illustration \n" +
             "            INNER JOIN is_agency_db.main_benifit as mi on mi.id = imi.id_main_benifit\n" +
             "\t\t\tLEFT JOIN contract as c on c.id_illustration = ill.id  \n" +
             "\t\t\twhere (ill.id_customer_info = ?1 and ill.create_time between ?2 and ?3 and c.id is null)\n" +
